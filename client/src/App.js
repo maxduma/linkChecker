678f930ddbd1c02c523b47259c3d87ee1cc53feb
Max <maxduma16@gmail.com>
2023-06-02 17:10:56 +0300

    included routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import { BrowserRouter} from 'react-router-dom';
 import 'materialize-css'
+import { useRoutes } from './routes';
 
 function App() {
+  const routes = useRoutes(false)
+
   return (
-    <div className="container">
-      <h1>Hello!</h1>
-    </div>
+    <BrowserRouter>
+     <div className="container">
+       {routes}
+     </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,33 +1,25 @@
 import React from "react"
-import {Switch, Route, Redirect} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import { LinksPage } from "./pages/LinksPage"
 import { CreatePage } from "./pages/CreatePage"
 import { DetailPage } from "./pages/DetailPage"
 import { AuthPage } from "./pages/AuthPage"
 
-export const useRoutes = isAuthenticated => {
+export const useRoutes = (isAuthenticated) => {
 	if(isAuthenticated) {
 		return (
-			<Switch>
-				<Route path="/links" exact>
-					<LinksPage />
-				</Route>
-				<Route path="/create" exact>
-					<CreatePage />
-				</Route>
-				<Route path="/detail/:id">
-					<DetailPage />
-				</Route>
-				<Redirect to="/create" />
-			</Switch>
+			<Routes>
+				<Route path="/links" element={<LinksPage />} />
+				<Route path="/create" element={<CreatePage />} />
+				<Route path="/detail/:id" element={<DetailPage />} />
+				<Route path="*" element={<Navigate replace to="/links" />} />
+			</Routes>
 		)
 	}
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<AuthPage  />
-			</Route>
-			<Redirect to="/" />
-		</Switch>
+		<Routes>
+			<Route path="/" element={<AuthPage />} />
+			<Route path="*" element={<Navigate replace to="/" />} />
+		</Routes>
 	)
 }
