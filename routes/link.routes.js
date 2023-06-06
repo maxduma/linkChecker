077cdcd01c4cca146d7dc76e07dd2bc0eb084bcd
Max <maxduma16@gmail.com>
2023-06-06 13:36:06 +0300

    fixed loader

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -2,7 +2,7 @@ const {Router} = require('express')
 const config = require('config')
 const shortid = require('shortid');
 const Link = require('../models/Link')
-const auth = require('../middleware/auth.middleware')
+const auth = require('./middleware/auth.middleware')
 const router = Router()
 
 router.post('/generate', auth, async (req, res) => {
